380a1c6143ef6dec3b3a3a165a8048009b9d8329
Patryk Palej <patrykpalej@gmail.com>
2021-11-01 16:43:20 +0100

    Consider changing deleting to truncating in tests

--- tests/integration/db/test_db_flow.py
+++ tests/integration/db/test_db_flow.py
@@ -1,6 +1,6 @@
 from dbutl.functions.create_single_table import create_single_table
 from dbutl.functions.insert_into_table import insert_into_table
-from dbutl.functions.delete_single_table import delete_single_table
+from dbutl.functions.truncate_single_table import truncate_single_table
 from dbutl.functions.drop_single_table import drop_single_table
 
 
@@ -33,7 +33,7 @@ def test_whole_db_flow():
     # TODO test select and update queries
 
     # DELETE
-    delete_single_table(test_table_config["name"])
+    truncate_single_table(test_table_config["name"])
 
     # DROP
     drop_single_table(test_table_config["name"])
